c60ec02f2e2efb0257a0547b6a57e1cb560c4d25
shgysd <shgysd@gmail.com>
2019-02-27 08:39:43 +0900

    Add propTypes for Congrats component

--- jotto/src/Congrats.test.js
+++ jotto/src/Congrats.test.js
@@ -2,17 +2,20 @@ import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
 
-import { findByClassName } from '../test/testUtils';
+import { findByClassName, checkProps } from '../test/testUtils';
 import Congrats from './Congrats';
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 
+const defaultProps = {success: false};
+
 const setup = (props={}) => {
-  return shallow(<Congrats {...props} />)
+  const setupProps = { ...defaultProps, ...props };
+  return shallow(<Congrats {...setupProps} />)
 }
 
 it('renders without error', () => {
-  const wrapper = setup();
+  const wrapper = setup({ success: false });
   const component = findByClassName(wrapper, 'component-congrats');
   expect(component.length).toBe(1);
 });
@@ -27,4 +30,9 @@ it('renders without error', () => {
   const wrapper = setup({success: true});
   const message = findByClassName(wrapper, 'congrats-message');
   expect(message.text().length).not.toBe(0);
+});
+
+it('does not throw warning with expected props', () => {
+  const expectedProps = { success: false };
+  checkProps(Congrats, expectedProps);
 });
--- jotto/test/testUtils.js
+++ jotto/test/testUtils.js
@@ -1,3 +1,14 @@
+import checkPropTypes from 'check-prop-types';
+
 export const findByClassName = (wrapper, val) => {
   return wrapper.find(`.${val}`)
+}
+
+export const checkProps = (component, conformingProps) => {
+  const propError = checkPropTypes(
+    component.propTypes,
+    conformingProps,
+    'prop',
+    component.name);
+  expect(propError).toBeUndefined();
 }
